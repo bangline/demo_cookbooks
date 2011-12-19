bc06247f56837f213d191a1f4aa63ff2d055aed0
Dave Kennedy <david@bangline.co.uk>
2011-12-19 02:17:22 +0000

    Add dependancies to rvm recipe

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -1,8 +1,8 @@
-%w(libreadline5-dev zlib1g-dev libssl-dev libxml2-dev libxslt1-dev).each do |pkg|
+%w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion).each do |pkg|
   package pkg
 end
  
 bash "install RVM" do
-  user "root"
-  code "bash < <( curl -s https://rvm.beginrescueend.com/install/rvm )"
+  user node[:user]
+  code "sudo bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )"
 end
